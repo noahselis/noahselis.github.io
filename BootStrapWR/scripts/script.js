bc3c3c01825cbbd2a84ebd9b8fa3c1b62053723c
Noah Selis <80969784+noahselis@users.noreply.github.com>
2021-12-01 10:48:37 -0700

    add files via upload

---
+++ BootStrapWR/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Noah Selis
+    File Name: script.js
+    Date: 12.01.21
+*/
+
+//jQuery for hero image to consume the header window space
+    $(document).ready(function(){
+    $('.hero').height($(window).height());
+    })
