051e03ad9db7e4ad9464f9d14e83618e07af6c0f
Noah Selis <80969784+noahselis@users.noreply.github.com>
2022-03-11 19:05:36 -0700

    Add files without node_modules

---
+++ swdv_142/block_02/project03/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer py-3 bg-light mt-5 br-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 text-muted">
+          <ul style="list-style: none">
+            <a href="#"><li>jonkerjanker@sweetnana.gov</li></a>
+            <a href="#"><li>420-555-0123</li></a>
+            <li>Call Or Email Anytime</li>
+          </ul>
+        </div>
+        <div class="col-md-6 text-muted">
+          We surely pride ourselves on something but I plumb forgot what it was.
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Foot-er",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ swdv_142/block_02/project03/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid my-5">
+    <a class="navbar-brand" href="#">The Vue Page</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Staff
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Farce McGilgwy</a></li>
+            <li><a class="dropdown-item" href="#">Coople Dreamshaft</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Aimee Ivankastein</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-3 w-100" type="search" placeholder="Vue Something Else?" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Go</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
