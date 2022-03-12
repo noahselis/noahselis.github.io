3812a016944331f883af784bfa1f7cdc7fbc5c35
Noah Selis <80969784+noahselis@users.noreply.github.com>
2022-03-11 18:59:27 -0700

    Testing File Uploads without node_modules

---
+++ swdv_142/block02/project_03/src/components/Cards.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card text-center">
+          <img
+            src="https://picsum.photos/id/69/200/200"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">This</h5>
+            <p class="card-text">
+              In the Big Rock Candy Mountains All the cops have wooden legs And
+              the bulldogs all have rubber teeth And the hens lay soft-boiled
+              eggs
+            </p>
+            <a href="#" class="btn btn-primary">Go there</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card text-center">
+          <img
+            src="https://picsum.photos/id/79/200/200"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">That</h5>
+            <p class="card-text">
+              In the Big Rock Candy Mountains The jails are made of tin And you
+              can walk right out again As soon as you are in
+            </p>
+            <a href="#" class="btn btn-primary">Go there</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card text-center">
+          <img
+            src="https://picsum.photos/id/89/200/200"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">The Other</h5>
+            <p class="card-text">
+              In the Big Rock Candy Mountains You never change your socks And
+              the little streams of alcohol Come trickling down the rocks
+            </p>
+            <a href="#" class="btn btn-primary">Go there</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card-s",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ swdv_142/block02/project_03/src/components/Jumbotron.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-7">
+        <img src="https://picsum.photos/id/237/600/400" alt="" />
+      </div>
+      <div class="col-md-5 text-center">
+        <h1 class="font-weight-light">Well Lemme Tell Ya...</h1>
+        <p class="mt-4">
+          Suspendisse ultricies gravida luctus. Quisque sit amet tincidunt ex,
+          non dignissim mauris. Vivamus eu enim cursus, vestibulum turpis et,
+          egestas justo. Pellentesque diam dui, porta at elit non, placerat
+          vestibulum massa. Quisque aliquet placerat eros in lacinia. Proin
+          tempor lectus ante. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Fusce accumsan ante egestas quam porta, et pulvinar neque
+          blandit. Nulla eu neque consequat, scelerisque mi nec, facilisis
+          ligula. Nullam accumsan scelerisque nunc non consequat. Aenean rutrum
+          tempus sollicitudin. Proin auctor tincidunt gravida. Duis euismod
+          dignissim quam. Sed elementum tristique molestie. Sed feugiat, diam
+          quis dictum tincidunt, turpis lorem varius quam, vel volutpat velit ex
+          vitae magna. Ut eu leo eros.
+        </p>
+        <button type="button" class="btn btn-primary">Annnnd Break!</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbo-tron",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
